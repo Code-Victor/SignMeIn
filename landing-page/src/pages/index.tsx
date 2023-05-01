34ec18ca2f2dad917ca9a1585b735ab581743586
Your name <oluwaborihamzat@gmail.com>
2023-05-01 10:51:46 +0100

    feat: set layout assigned to me

--- landing-page/src/pages/index.tsx
+++ landing-page/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Hero, Navbar } from "@/components";
+import { Hero, Navbar, Cta, Footer } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,8 +9,10 @@ export default function Home() {
     <>
       <Navbar />
       <main>
-        <Hero/> 
+        <Hero />
+        <Cta />
       </main>
+      <Footer />
     </>
   );
 }
