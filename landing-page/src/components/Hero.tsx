985e90b4f903888d669958cd8b911952842d1df6
Your name <oluwaborihamzat@gmail.com>
2023-05-01 10:49:08 +0100

    feat: add responsiveness to hero

--- landing-page/src/components/Hero.tsx
+++ landing-page/src/components/Hero.tsx
@@ -5,21 +5,43 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div className="bg-primary min-h-[calc(100vh_-_96px)] flex items-center justify-center relative">
-        <Image src="/img/qr-1.png" alt="sample qr code" className="absolute top-1/2 left-0" width={100} height={200}/>
-        <Image src="/img/qr-2.png" alt="sample qr code" className="absolute top-1/2 right-0" width={129} height={280} />
-        <Image src="/img/wave.png" alt="wave" className="absolute" objectFit="cover" layout="fill" />
-      <div >
-        <h1 className="text-white text-center text-6xl font-bold max-w-[1140px] mx-auto">
+    <div className="bg-primary min-h-[calc(100vh_-_96px)] px-5 flex items-center justify-center relative isolate">
+      <Image
+        src="/img/qr-1.png"
+        alt="sample qr code"
+        className="absolute top-0 md:top-1/2 -z-[1] left-0"
+        width={100}
+        height={200}
+      />
+      <Image
+        src="/img/qr-2.png"
+        alt="sample qr code"
+        className="absolute bottom-0  -z-[1]  md:top-1/2 right-0"
+        width={129}
+        height={280}
+      />
+      <Image
+        src="/img/wave.png"
+        alt="wave"
+        className="absolute -z-[2]"
+        objectFit="cover"
+        layout="fill"
+      />
+      <div className="z-2">
+        <h1 className="text-white text-center text-4xl md:text-6xl font-bold max-w-[1000px] mx-auto">
           Effortless Attendance Tracking with QR Codes
         </h1>
-        <p className="mt-4 text-lg text-white text-center max-w-[630px] mx-auto">
+        <p className="mt-4 text-base md:text-lg text-white text-center max-w-[630px] mx-auto">
           Effortlessly track attendance with our QR-code software. Quick scans
           record and organize data in real-time, saving you time and effort.
         </p>
         <div className="mt-8 flex gap-4 justify-center">
-          <Button color="light" size="lg">For organisation</Button>
-          <Button color="light" size="lg">For Worker</Button>
+          <Button color="light" size="lg">
+            For organisation
+          </Button>
+          <Button color="light" size="lg">
+            For Worker
+          </Button>
         </div>
       </div>
     </div>
