e1bcda0ad32c6297282dea6f2d5fc90a479162e4
AbdulAfeez Adeyemo <114268023+Abdul-Afeez99@users.noreply.github.com>
2023-06-13 12:08:17 +0100

    Update settings.py

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -169,7 +169,7 @@ CORS_ORIGIN_WHITELIST = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=90),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
